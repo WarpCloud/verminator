5ee9c89774a3f68100b0a1ebcf25ccf28dfee002
Xiaming Chen <xiaming.chen@transwarp.io>
2019-03-27 17:07:00 +0800

    Remove validation from genver and oem

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 
 
 def walk_path_files(directory, target_folder=None):
--- testing.sh
+++ testing.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pip install -r tests/requirements.txt -i http://172.16.1.161:30033/repository/pypi/simple/ --trusted-host 172.16.1.161
+
 nosetests --with-coverage --cover-package=verminator \
     --cover-erase --cover-tests --nocapture \
     -d --all-modules \
